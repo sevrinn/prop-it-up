fd08fa7c4d9eea725f7124f8abf8717f724c0683
Sevrinn <sevrinnsevrinn@gmail.com>
2021-07-08 13:32:48 -0700

    idestructured props object of PersonCard.js

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Person = (props) => {
+  const {firstName, lastName, age, hairColor} = props;
   return (
     <div>
-      <h2>{props.lastName},{props.firstName}</h2>
-      <p>Age: {props.age}</p>
-      <p>Hair Color: {props.hairColor}</p>
+      <h2>{lastName},{firstName}</h2>
+      <p>Age: {age}</p>
+      <p>Hair Color: {hairColor}</p>
     </div>
   )
 }
