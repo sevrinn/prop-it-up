b073a3d39156f665074129687eb1a75c79e87eb4
Sevrinn <sevrinnsevrinn@gmail.com>
2021-07-08 12:20:27 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import Person from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <Person 
+        firstName ={"Jane"}
+        lastName ={"Doe"}
+        age={45}
+        hairColor={"Black"} />
+
+      <Person 
+        firstName ={"Jon"}
+        lastName ={"Smith"}
+        age={88}
+        hairColor={"Brown"} />
+
+      <Person 
+        firstName ={"Millard"}
+        lastName ={"Fillmore"}
+        age={50}
+        hairColor={"Brown"} />
+
+      <Person 
+        firstName ={"Maria"}
+        lastName ={"Smith"}
+        age={62}
+        hairColor={"Brown"} />
+    </div>
+
+    
+  );
+}
+
+export default App;
