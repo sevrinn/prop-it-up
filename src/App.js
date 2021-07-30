a965f6c2f5bd310a3ae8f8c452190ddf4be4d3eb
Sevrinn <sevrinnsevrinn@gmail.com>
2021-07-30 15:55:33 -0700

    commented code for clarity

--- src/App.js
+++ src/App.js
@@ -34,3 +34,16 @@ function App() {
 }
 
 export default App;
+
+/**
+ * Code Breakdown
+ * 1. imports the css file holding all the styles
+ * 2. imports Person component from the PersonCard.js
+ * 
+ * 5. This app returns a div containing 4 Person components, each passing different information in as props.
+ * 
+ * 36. exports App
+ * 
+ * It's important to note that a reference to App is placed in the ReactDOM.render
+ * api method in the index.js file where it's rendered in the root div on the index.html
+ */
